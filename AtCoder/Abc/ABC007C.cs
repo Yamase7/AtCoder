fc76e93cd320bc1226f2af06cec9e4e1e8bae542
Seiya Yamaguchi <pierrot_service@icloud.com>
2025-01-01 11:33:08 +0900

    Add ABC007C problem.

---
+++ AtCoder/Abc/ABC007C.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtCoder.Abc
+{
+    // https://atcoder.jp/contests/abc104/tasks/abc007_c
+    public class ABC007C
+    {
+        public static void Main(string[] args)
+        {
+            var sw = new System.IO.StreamWriter(Console.OpenStandardOutput()) { AutoFlush = false };
+            Console.SetOut(sw);
+
+            // 整数配列の入力
+            var work = Console.ReadLine().Split(' ').Select(i => long.Parse(i)).ToArray();
+            var r = work[0];
+            var c = work[1];
+
+            work = Console.ReadLine().Split(' ').Select(i => long.Parse(i)).ToArray();
+            (long x, long y) s = (work[1], work[0]);
+
+            work = Console.ReadLine().Split(' ').Select(i => long.Parse(i)).ToArray();
+            (long x, long y) g = (work[1], work[0]);
+
+            // マップの取得
+            var map = new char[r, c];
+            var counter = new long[r, c];
+            for (int i = 0; i < r; i++)
+            {
+                string data = Console.ReadLine();
+                for (int j = 0; j < data.Length; j++)
+                {
+                    map[i, j] = data[j];
+                }
+            }
+
+            var queue = new Queue<(long x, long y)>();
+
+            // 開始地点を登録
+            queue.Enqueue((s.x-1, s.y-1));
+            map[s.y - 1, s.x - 1] = 'o';
+            counter[s.y - 1, s.x - 1] = 0;
+
+            // 次に移動する座標の計算用配列
+            var d = new (long x, long y)[]
+            {
+                (1, 0),
+                (0, 1),
+                (-1, 0),
+                (0, -1),
+            };
+
+            bool outOfMap(long x, long y) => x < 0 || x >= c || y < 0 || y >= r;
+            bool isWall(long x, long y) => map[y, x] == '#';
+            bool isChecked(long x, long y) => map[y, x] == 'x' || map[y, x] == 'o';
+
+            while (queue.Count > 0)
+            {
+                var current = queue.Dequeue();
+
+                // 現在地を通過済みとして記録
+                map[current.y, current.x] = 'x';
+
+                // 現在地の四方を確認する
+                for (int i = 0; i < 4; i++)
+                {
+                    // 確認先座標
+                    (long x, long y) newXY = (current.x + d[i].x, current.y + d[i].y);
+
+                    // 確認先がマップ外だったらスキップ
+                    if (outOfMap(newXY.x, newXY.y))
+                        continue;
+
+                    // 確認先が壁/予約/通過済みであればスキップ
+                    if (isWall(newXY.x, newXY.y) || isChecked(newXY.x, newXY.y))
+                        continue;
+
+                    // 確認先を通過予定とし歩数を記録する
+                    map[newXY.y, newXY.x] = 'o';
+                    queue.Enqueue(newXY);
+                    counter[newXY.y, newXY.x] = counter[current.y, current.x] + 1;
+                }
+            }
+
+            string result = $"answer: {counter[g.y-1, g.x-1]}";
+
+            Console.WriteLine(result);
+
+            Console.Out.Flush();
+        }
+    }
+}
---
+++ AtCoder/Lib/Search.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtCoder.Lib
+{
+    public static class Search
+    {
+        // 目的地にたどり着けるかどうか
+        public static bool DFS(char[,] map, long h, long w)
+        {
+            if (h < 0 || h >= map.GetLength(0))
+                return false;
+            if (w < 0  || w >= map.GetLength(1))
+                return false;
+
+            switch (map[h, w])
+            {
+                // ゴールだったら
+                case 'g':
+                    // 探索終了
+                    return true;
+
+                // すでに通った道か壁だったら
+                case 'd':
+                case '#':
+                    // 元の道に戻って探索再開
+                    return false;
+
+                // 開始位置か道だったら
+                case 's':
+                case '.':
+                    // たどり着いたことを記録して
+                    map[h, w] = 'd';
+
+                    // 次の道へ探索する
+                    return DFS(map, h + 1, w)
+                        || DFS(map, h - 1, w)
+                        || DFS(map, h, w + 1)
+                        || DFS(map, h, w - 1);
+
+                default:
+                    throw new ArgumentException();
+            }
+        }
+
+        public static void BitSearch(string s)
+        {
+            int n = s.Length - 1;
+            for (int bit = 0; bit < 1 << n; bit++)
+            {
+
+            }
+        }
+    }
+}
