fe9cd474d0e00b8289a1326ec29cfbee6ed11b39
Seiya Yamaguchi <pierrot_service@icloud.com>
2025-01-03 16:27:21 +0900

    Add greedy problems.

---
+++ AtCoder/JOI/JOI2007A.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtCoder.Joi
+{
+    // https://atcoder.jp/contests/joi2008yo/tasks/joi2008yo_a
+    public class JOI2007A
+    {
+        public static void Main(string[] args)
+        {
+            var sw = new System.IO.StreamWriter(Console.OpenStandardOutput()) { AutoFlush = false };
+            Console.SetOut(sw);
+
+            // 整数の入力
+            long n = long.Parse(Console.ReadLine());
+
+            var change = 1000 - n;
+            var count = 0;
+
+            int[] coins = new int[6] { 500, 100, 50, 10, 5, 1 };
+
+            // 全硬貨確認する
+            foreach (var coin in coins)
+            {
+                // その硬貨をおつりとして出す
+                while (change >= coin)
+                {
+                    count++;
+                    change -= coin;
+                }
+            }
+
+            string result = $"{count}";
+
+            Console.WriteLine(result);
+
+            Console.Out.Flush();
+        }
+    }
+}
---
+++ AtCoder/Keyence/Keyence2020B.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtCoder.Keyence
+{
+    // https://atcoder.jp/contests/keyence2020/tasks/keyence2020_b
+    public class Keyence2020B
+    {
+        public static void Main(string[] args)
+        {
+            var sw = new System.IO.StreamWriter(Console.OpenStandardOutput()) { AutoFlush = false };
+            Console.SetOut(sw);
+
+            // データの入力
+            long n = long.Parse(Console.ReadLine());
+            var robots = new (long x, long l)[n];
+            for (int i = 0; i < n; i++)
+            {
+                var input = Console.ReadLine().Split(' ').Select(str => long.Parse(str)).ToArray();
+                robots[i] =(input[0], input[1]);
+            }
+
+            long counter = 0;
+
+            // 右腕の先端の位置でソートする
+            (long left, long right)[] sortedByT = robots
+                .Select(robot => (robot.x - robot.l, robot.x + robot.l))
+                .OrderBy(robot => robot.Item2).ToArray();
+
+            // 現在地を最低値にする
+            long current = int.MinValue;
+
+            for (int i = 0; i < n; i++)
+            {
+                // 現在位置が次のロボットの左手と重ならなかったら
+                if (current <= sortedByT[i].left)
+                {
+                    // カウントアップ
+                    counter++;
+
+                    // ロボットの右手位置を現在地として記録する
+                    current = sortedByT[i].right;
+                }
+            }
+
+            string result = $"{counter}";
+
+            Console.WriteLine(result);
+
+            Console.Out.Flush();
+        }
+    }
+}
